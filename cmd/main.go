b0b8e46f2af31fb2f7866cc0654034ec70be2714
omelchenko.ma <omelchenko.ma@dvfu.ru>
2026-02-11 06:53:47 +1000

    First steps

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
-	"domain"
+
+	"github.com/E-n-d-l-e-s-s-A-I/vsixctl/internal/domain"
 )
 
 func main() {
-	fmt.Println("hello word")
+	extension := domain.Extension{
+		ID:      domain.ExtensionID{},
+		Version: "1.31.3",
+	}
+	fmt.Println(extension)
 }
